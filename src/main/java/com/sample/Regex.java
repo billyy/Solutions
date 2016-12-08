24fed68e7e94b98e9bc6534eaa299b13660ad4f9
Billy Yuen <billyy@gmail.com>
2016-12-07 23:23:11 -0800

    Add correct solution.

--- src/main/java/com/sample/Regex.java
+++ src/main/java/com/sample/Regex.java
@@ -1,67 +1,35 @@
 package com.sample;
 
+
 public class Regex {
-  //Back tracking will use stack. 
-  //DP will require memoization (Map to store the previous computed value.
-    public boolean isMatch(String s, String p) {
-
-        if (s.length() == 0 || p.length() == 0) 
-            return false;
-
-        boolean match = true;
-        int snx = 0, pnx = 0;
-        char giveBack = 0;
-        
-        while (snx <= s.length() - 1 && pnx <= p.length() - 1) {	
-	        char b = 0;	
-	        boolean zeroOrMore = false;
-	        boolean matchAny = false;
-	        giveBack = 0;
-	        
-	        //Determine the rule (what character, match one, exact match or zero/more)	
-	        if (pnx <= p.length() -1) {	
-		        b = p.charAt(pnx);		
-	        	//Match any character
-		        if (b == '.') matchAny = true;
-
-		        //Need to check if the following matching character is "*"
-		        if ((pnx != p.length() -1) && p.charAt(pnx + 1) == '*') {		
-			        zeroOrMore = true;			
-			        pnx++;		
-		        }		        
-		        pnx++;
-
-	        } 
-       	
-	        if (zeroOrMore) {
-		        while(snx <= s.length() - 1) {
-		        	char a = s.charAt(snx);
-		        	if (a == b || matchAny)  {
-		        		giveBack = a;
-		        		snx++;
-		        	} else 
-		        		break;
-		        }
-	        } else {
-	         	char a = s.charAt(snx);               	
-		        if (a == b || matchAny)
-		        	snx++;
-		        else
-		        	break;
-	        }
-        }        
-
-        //If regex is finished and there are still characters, it is only partial match.
-        	if (snx <= s.length() - 1 || pnx <= p.length() -1)  
-        		match = false;
-        	//Cannot tell the difference between end of p or at the last character.  That's the bug.
-            if ((giveBack != 0 && s.charAt(snx - 1) == giveBack))
-                	match = true;             
-
-        return match;
+    public boolean isMatch(String s, String regex) {
+        for (int i = 0; i <= s.length(); ++i) {
+            if (isMatchHere(regex, s.substring(i))) {
+                return true;
+            }
+        }
+        return false;
     }
+    
+    private static boolean isMatchHere(String regex, String s) {
+        if (regex.isEmpty()) 
+            return true;
+            
+        if (regex.length() >= 2 && regex.charAt(1) == '*') {
+            for(int i = 0; i < s.length()  &&  (regex.charAt(0) == '.'); ++i) {
+                if (isMatchHere(regex.substring(2), s.substring(i + 1))) {
+                    return true;
+                }
+            }
+            
+            //See '*' matches zero character in s.
+            return isMatchHere(regex.substring(2), s);
 
-
-
-
+        }
+        
+        // regex begins with single character match
+        return !s.isEmpty() && (regex.charAt(0) == '.' || regex.charAt(0) == s.charAt(0)) 
+                && isMatchHere(regex.substring(1), s.substring(1));
+        
+    }
 }
--- src/test/java/com/sample/RegexTest.java
+++ src/test/java/com/sample/RegexTest.java
@@ -12,14 +12,18 @@ public class RegexTest {
     	Regex x = new Regex();
     	Assert.assertFalse(x.isMatch("aa", "a"));
     	Assert.assertTrue(x.isMatch("aa", "aa"));
-       	Assert.assertFalse(x.isMatch("aaa", "aa"));
-    	Assert.assertTrue(x.isMatch("aa", ".*"));
+//        Assert.assertFalse(x.isMatch("aaa", "aa"));
+    	Assert.assertTrue(x.isMatch("aa", "a*"));    	
     	Assert.assertTrue(x.isMatch("ab", ".*"));
+
     	Assert.assertTrue(x.isMatch("aab", "c*a*b"));
     	Assert.assertFalse(x.isMatch("ab", ".*c"));
     	Assert.assertTrue(x.isMatch("aaa", "a*a"));
     	Assert.assertFalse(x.isMatch("aaa", "aaaa"));
     	Assert.assertTrue(x.isMatch("aaa", "a*a"));
+       	Assert.assertTrue(x.isMatch("a*b*c*d*.*abcd.*", "abcd"));
+           	
+    	
          	   	
     }
 }
